f3ddd7203106ebf6b15ee68774a6ef234da3a1a0
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-07 18:35:08 +0300

    create a function for the winner of the game

--- game.py
+++ game.py
@@ -35,7 +35,35 @@ class TicTacToe:
                 self.current_winner = letter
             return True
         return False
+    
+    def winner(self, square, letter):
+        # winner if 3 in a row anywhere, we have to check all of these
+        # first lets check the row
+        row_ind = square // 3
+        row = self.board[row_ind*3 : (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
 
+        #check the colum next
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True   
+        
+        # check diagonals
+        # but only if the square is an even number (0, 2, 4, 6, 8)
+        # these are the only moves possible to win a diagonal
+        if square  % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]  # left to right diagonal
+            if all([spot == letter for spot in diagonal1]):
+                return True   
+            diagonal2 = [self.board[i] for i in [2, 4, 6]] # right to left diagonal
+            if all([spot == letter for spot in diagonal2]):
+                return True 
+
+        # if all these checks fail
+        return False
+      
 def play(game, x_player, o_player, print_game=True):
     # return the winner of the game! of None for a tie
     if print_game: 
