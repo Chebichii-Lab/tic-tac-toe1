aa4f46ef2174a583db182f72241ead5b7d083f7d
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-08 21:21:32 +0300

    make correction

--- game.py
+++ game.py
@@ -101,7 +101,7 @@ def play(game, x_player, o_player, print_game=True):
             print('It\'s a tie')
 
 
-if  __name__ == '__main':
+if  __name__ == '__main__':
     x_player = HumanPlayer('X')
     o_player = RandomComputerPlayer('O')
     t = TicTacToe()
